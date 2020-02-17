f714bc1c2aef7c42e8b8fe3abdb31b512b1c731c
philip jansson <phille00@hotmail.se>
2020-02-17 12:35:14 +0100

    GameEntity

---
+++ CodePC/MainProgram/GameEntity.cpp
@@ -0,0 +1,45 @@
+#include "GameEntity.h"
+
+GameEntity::GameEntity()
+{
+}
+
+GameEntity::~GameEntity()
+{
+}
+
+void GameEntity::takeDamage(int damage)
+{
+}
+
+int GameEntity::getHealth()
+{
+	return 0;
+}
+
+void GameEntity::gainHelth()
+{
+}
+
+
+bool GameEntity::attackCooldown()
+{
+	return false;
+}
+
+void GameEntity::attack(GameEntity* enemy, float range)
+{
+}
+
+void GameEntity::move(int x, int y)
+{
+}
+
+int GameEntity::getMovingSpeed()
+{
+	return 0;
+}
+
+void GameEntity::draw(sf::RenderTarget& target, sf::RenderStates state)
+{
+}
---
+++ CodePC/MainProgram/GameEntity.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <string>
+
+using namespace std;
+class GameEntity : public sf::Drawable
+{
+private:
+	sf::Sprite sprite;
+	sf::Texture texture;
+	int movingSpeed;
+	int health;
+	bool canAttack;
+
+public:
+	GameEntity(string textureName, int movingSpeed, int health, );
+	GameEntity();
+	~GameEntity();
+
+	void takeDamage(int damage);
+	int getHealth();
+	void gainHelth();
+	bool attackCooldown();
+	void attack(GameEntity *enemy, float range);
+	void move(int x, int y);
+	int getMovingSpeed();
+	virtual void die() = 0;
+
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates state);
+
+
+};
+
