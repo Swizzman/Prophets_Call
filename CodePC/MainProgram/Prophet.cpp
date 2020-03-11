fc637117f103bf304c2aa24a77896fdc98b04272
oliver johansson <swizzman123@gmail.com>
2020-03-11 16:01:35 +0100

    Revert "adws"
    This reverts commit 6744b35321916ec59094a4a425fa19b4c04daa45.

--- CodePC/MainProgram/Prophet.cpp
+++ CodePC/MainProgram/Prophet.cpp
@@ -7,7 +7,7 @@ sf::Vector2f Prophet::getCircCenter() const
 }
 
 Prophet::Prophet() :
-	GameEntity("Prophet.png", 0, 0, 1000)
+	GameEntity("thisProphetSpriteSheet.png", 0, 0, 1000, true)
 {
 
 	//this->group1 = new group();
@@ -29,15 +29,16 @@ Prophet::Prophet() :
 	this->convertingSpeed = 10;
 	this->chosenGroup = 0;
 	this->collectedSouls = 0;
-	setPosition(100, 100);
 	this->otherProphet = nullptr;
 	this->convertCirc.setRadius(150);
 	this->convertCirc.setFillColor(sf::Color::Transparent);
 	this->convertCirc.setOutlineColor(sf::Color::Red);
 	this->convertCirc.setOutlineThickness(5.f);
-	this->convertCirc.setOrigin(getOrigin());
-	currentCommandGroup = 0;
+	this->convertCirc.setOrigin(getPosition().x + getBounds().width/2 , getPosition().y + getBounds().height/2);
 
+	currentCommandGroup = 0;
+	setPosition(100, 100);
+	
 
 }
 
@@ -109,6 +110,47 @@ void Prophet::convertsFollow()
 	if (otherProphet != nullptr)
 	{
 		commandMan.useCommand();
+		if (abs(this->getMovingSpeedX()) > abs(this->getMovingSpeedY()) && this->getMovingSpeedX() > 0)
+		{
+			startAnimation((int)FOLLOWERSPRITEROW::WALKINGRIGHT, 9, 15, 0);
+			lastWalkingDirection = (int)FOLLOWERSPRITEROW::WALKINGRIGHT;
+			cout << abs(this->getMovingSpeedX()) << " : " << abs(this->getMovingSpeedY()) << endl;
+		}
+		else if (abs(this->getMovingSpeedX()) > abs(this->getMovingSpeedY()) && this->getMovingSpeedX() < 0)
+		{
+			startAnimation((int)FOLLOWERSPRITEROW::WALKINGLEFT, 9, 15, 0);
+			lastWalkingDirection = (int)FOLLOWERSPRITEROW::WALKINGLEFT;
+		}
+		else if (abs(this->getMovingSpeedX()) < abs(this->getMovingSpeedY()) && this->getMovingSpeedY() > 0)
+		{
+			startAnimation((int)FOLLOWERSPRITEROW::WALKINGDOWN, 9, 15, 0);
+			lastWalkingDirection = (int)FOLLOWERSPRITEROW::WALKINGDOWN;
+		}
+		else if (abs(this->getMovingSpeedX()) < abs(this->getMovingSpeedY()) && this->getMovingSpeedY() < 0)
+		{
+			startAnimation((int)FOLLOWERSPRITEROW::WALKINGUP, 9, 15, 0);
+			lastWalkingDirection = (int)FOLLOWERSPRITEROW::WALKINGUP;
+		}
+
+		if (abs(getMovingSpeedX()) == abs(getMovingSpeedY()) && this->getMovingSpeedY() < 0)
+		{
+			//	cout << "same walking speed " << endl;
+			startAnimation((int)FOLLOWERSPRITEROW::WALKINGUP, 9, 15, 0);
+			lastWalkingDirection = (int)FOLLOWERSPRITEROW::WALKINGUP;
+		}
+		else if (abs(getMovingSpeedX()) == abs(getMovingSpeedY()) && this->getMovingSpeedY() > 0)
+		{
+			startAnimation((int)FOLLOWERSPRITEROW::WALKINGDOWN, 9, 15, 0);
+			lastWalkingDirection = (int)FOLLOWERSPRITEROW::WALKINGDOWN;
+		}
+		else if (getMovingSpeedX() == 0 && getMovingSpeedY() == 0)
+		{
+			startAnimation(lastWalkingDirection, 1, 15, -1);
+
+		}
+
+
+		updateAnimation();
 	}
 
 }
@@ -156,35 +198,55 @@ void Prophet::moveProphet()
 {
 	int hDir = 0;
 	int vDir = 0;
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-	{
-		vDir = -this->ySpeed;
-	}
-	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-	{
-		vDir = this->ySpeed;
-
-	}
-	else
-	{
-		vDir = 0;
-	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-	{
-		hDir = this->xSpeed;
-	}
-	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+	if (getCurrentPriority() <= 0)
 	{
 
-		hDir = -this->xSpeed;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		{
+			hDir = this->xSpeed;
+			startAnimation((int)ANIMATIONSPRITEROW::WALKINGRIGHT, 9, 10,0);
+			lastWalkingDirection = (int)ANIMATIONSPRITEROW::WALKINGRIGHT;
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+		{
+
+			hDir = -this->xSpeed;
+			startAnimation((int)ANIMATIONSPRITEROW::WALKINGLEFT, 9, 10,0);
+			lastWalkingDirection = (int)ANIMATIONSPRITEROW::WALKINGLEFT;
+		}
+		else
+		{
+			hDir = 0;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+		{
+			vDir = -this->ySpeed;
+			startAnimation((int)ANIMATIONSPRITEROW::WALKINGUP,9,10,0);
+			lastWalkingDirection = (int)ANIMATIONSPRITEROW::WALKINGUP;
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+		{
+			vDir = this->ySpeed;
+			startAnimation((int)ANIMATIONSPRITEROW::WALKINGDOWN, 9, 10,0);
+			lastWalkingDirection =  (int)ANIMATIONSPRITEROW::WALKINGDOWN;
+		}
+		else
+		{
+			vDir = 0;
+		
+		}
 	}
-	else
+	if (hDir == 0 && vDir == 0)
 	{
-		hDir = 0;
+
+		startAnimation(lastWalkingDirection, 1, 30,-1);
 	}
+	updateAnimation();
 	setMovingSpeed(hDir, vDir);
 	move();
-	convertCirc.setPosition(getBounds().left - getBounds().width, getBounds().top - getBounds().height - 20);
+	
+	convertCirc.setPosition(getPosition().x -convertCirc.getRadius()/2-11, getPosition().y - convertCirc.getRadius() /2-11);
+	
 
 }
 
@@ -396,6 +458,7 @@ bool Prophet::getIfAbilityIsActive()
 	}
 	else
 	{
+		abilityAnimation = false;
 		return false;
 	}
 }
@@ -404,6 +467,11 @@ void Prophet::timerForAbility()
 {
 	if (otherProphet != nullptr)
 	{
+		if (abilityAnimation == false)
+		{
+			//startAnimation(lastWalkingDirection-4, 7,15,1);
+			abilityAnimation = true;
+		}
 
 		abilityMan.stopAbility();
 		abilityMan.whileAbilityIsActive();
@@ -427,6 +495,11 @@ bool Prophet::returnReinforceBool()
 		return abilityMan.returnReinforcementBool();
 	}
 
+}
+
+int Prophet::getWalkingDirection()
+{
+	return lastWalkingDirection;
 }
 
 void Prophet::addFollower(Follower* follower)
