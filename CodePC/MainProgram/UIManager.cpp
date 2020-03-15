6283a406dc529b21353d3901f2facb57c74577b6
philip jansson <phille00@hotmail.se>
2020-03-15 22:50:06 +0100

    asd

--- CodePC/MainProgram/UIManager.cpp
+++ CodePC/MainProgram/UIManager.cpp
@@ -328,8 +328,9 @@ void UIManager::removeFps(int followersInGroup, int whichGroup, int health)
 		{
 			if (fps[i] != nullptr)
 			{
-
+				std::cout <<  "this is place i in array "<< i << std::endl: 
 				delete fps[i];
+
 			}
 		}
 
